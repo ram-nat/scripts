a34628193062a1c6b10767da282e5c24d23f475a
Ram Natarajan <ram@nramkumar.org>
2025-11-10 16:04:04 -0800

    Update fix msedge app icons script

--- fix_msedge_icons.sh
+++ fix_msedge_icons.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 app_dir="${HOME}/.local/share/applications"
+app_backup_dir="${HOME}/msedge-apps"
+
+if [ ! -d "${app_backup_dir}" ]; then
+  echo "Creating ${app_backup_dir}"
+  mkdir -p "${app_backup_dir}"
+fi
 
 for file in "$app_dir"/msedge-*; do
+  cp "${file}" "${app_backup_dir}/"
   filename=$(basename "$file")
   rest="${filename#msedge-}"
   rest="${rest%%.*}"
-  wmclass="msedge-_$rest"
-  sed -i "/^StartupWMClass=/c\StartupWMClass=$wmclass" "$file"
+  new_filename="msedge-_${rest}.desktop"
+  mv "${file}" "$app_dir/${new_filename}"
+  #wmclass="msedge-_$rest"
+  #sed -i "/^StartupWMClass=/c\StartupWMClass=$wmclass" "$file"
 done
